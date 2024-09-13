6344e3399a811a11a4850ce051bb24a864b9e4cb
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-09-13 15:58:18 -0300

    add: register form, validation and actions

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,8 +1,9 @@
+import { RegisterForm } from "@/components/auth";
 
 export default function RegisterPage() {
   return (
     <div>
-      <h1>Hello Page</h1>
+      <RegisterForm />
     </div>
   );
 }
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -2,5 +2,6 @@ export { BackButton } from './BackButton';
 export { Header } from './Header';
 export { LoginButton } from './LoginButton';
 export { LoginForm } from './LoginForm';
+export { RegisterForm } from './RegisterForm';
 export { CardWrapper } from './CardWrapper'
 export { Social } from './Social';
