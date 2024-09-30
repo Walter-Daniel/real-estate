ec9549f0782e13e017d7baa20d3742880243aa68
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-09-29 23:20:59 -0300

    add: location page

--- src/app/(routes)/create/[id]/address/page.tsx
+++ src/app/(routes)/create/[id]/address/page.tsx
@@ -1,8 +1,14 @@
 
+import { FormLocation } from "@/app/(routes)/_components";
+
 export default function AddressPage() {
+
   return (
     <div>
-      <h1>Hello Page</h1>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">Dónde está ubicada tu propiedad?</h2>
+      </div>
+      <FormLocation />
     </div>
   );
 }
