300c2018837254e602a008f070ceadd19c402b4a
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-10-02 17:22:49 -0300

    add: listing card

---
+++ src/actions/home/getProperties.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { db } from '@/lib/db';
+
+export const getProperties = async() => {
+    try {
+        const properties = await db.home.findMany({
+            where: {
+                addedLocation: true,
+                addedCategory: true,
+                addedDescription: true
+            },
+            select: {
+                photo: true,
+                id: true,
+                price: true,
+                title: true
+            }
+        });
+
+        return properties || [];
+    } catch (error) {
+        console.log('[GET-PROPERTIES]: ', error)
+        return []
+    }
+}
