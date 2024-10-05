8b488f97dce6ffee07561e16d9b418d9e62b4f1a
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-10-05 20:57:49 -0300

    fix: gethouses

--- src/app/(routes)/house/[id]/page.tsx
+++ src/app/(routes)/house/[id]/page.tsx
@@ -1,9 +1,13 @@
+import { getHouse } from "@/actions/property/get-house";
+
 interface HousePageProps {
     params: {
         id: string
     }
 }
-export default function HousePage({params}: HousePageProps) {
+export default async function HousePage({params}: HousePageProps) {
+  const house = await getHouse(params.id);
+  console.log({house})
   return (
     <div>
       <h1>Hello Page {params.id}</h1>
