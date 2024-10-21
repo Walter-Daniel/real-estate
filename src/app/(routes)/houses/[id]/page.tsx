99307315bc810df5db8e60d85874d25524f25410
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-10-21 15:13:28 -0300

    add: carousel, description and price to house page

--- src/app/(routes)/houses/[id]/page.tsx
+++ src/app/(routes)/houses/[id]/page.tsx
@@ -1,15 +1,39 @@
-import { getHouse } from "@/actions/property/get-house";
+import { getHouseById } from "@/actions";
+import { redirect } from "next/navigation";
+import { CarouselImages } from "./ui/Carousel";
+import { formatPriceARS } from "@/helpers/formatPriceArg";
 
 interface HousePageProps {
-    params: {
-        id: string
-    }
+  params: {
+    id: string;
+  };
 }
-export default async function HousePage({params}: HousePageProps) {
-  const house = await getHouse(params.id);
+export default async function HousePage({ params }: HousePageProps) {
+  const {ok, house} = await getHouseById(params.id);
+  if(!ok || !house){
+    redirect('/');
+  }
+  console.log({ house });
   return (
     <div>
-      <h1>Hello Page {params.id}</h1>
+    
+      <div className="grid grid-cols-12 gap-4 w-full">
+        <section className="col-span-7">
+          <CarouselImages images={house.HouseImage}/>
+          <p>habitaciones, visitantes, baños</p>
+        </section>
+        <section className="col-span-5 px-2">
+        <h2 className="text-2xl font-semibold tracking-tight transition-colors">
+        {house?.title}
+      </h2>
+          <p className="text-xl mb-2">{formatPriceARS(house.price)} x noche</p>
+          <p>
+            <span className="font-semibold ">Descripción: </span>
+            {house.description}</p>
+          <p>mapa</p>
+          <p>Alquilar</p>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/app/(routes)/houses/[id]/ui/Carousel.tsx
@@ -0,0 +1,42 @@
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+
+interface HouseImage {
+    url: string
+}
+
+interface Props {
+  images: HouseImage[]
+}
+
+export function CarouselImages({ images }: Props) {
+  return (
+    <Carousel className=" mx-10">
+      <CarouselContent>
+        {images.map((image) => (
+          <CarouselItem key={image.url}>
+            <div className="p-1 w-full">
+              <Image 
+                src={image.url}
+                alt="Imagenes de la casa"
+                width={1200}
+                height={800}
+                className="h-[90vh] object-cover"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
